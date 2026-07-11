e050001e0e951e253decb1fe8b9fa099575cacca
infreeJ <kasious97@naver.com>
2026-07-11 16:36:54 +0900

    String - 기본

---
+++ src/lang/string/CharArrayMain.java
@@ -0,0 +1,11 @@
+package lang.string;
+
+public class CharArrayMain {
+    public static void main(String[] args) {
+        char[] a = new char[]{'h', 'e', 'l', 'l', 'o'};
+        System.out.println(a);
+
+        String str = "hello";
+        System.out.println(str);
+    }
+}
